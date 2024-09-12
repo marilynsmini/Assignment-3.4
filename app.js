4fab7b41cfa6bd96ad2eaeaaa5f51458ede90f16
marilynsmini <122817758+marilynsmini@users.noreply.github.com>
2024-09-12 18:37:44 -0500

    initial

---
+++ app.js
@@ -0,0 +1,30 @@
+const name = "Marilyns Project";
+
+function toUpper(text) {
+    const upperCase = text.toUpperCase();
+    console.log(upperCase);
+}
+function add(num1, num2, num3) {
+    console.log(num1 + num2 + num3);
+}
+const cat = {
+name: "Oscar",
+age: 12,
+breed: "tabby",
+color: "grey",
+}
+console.log(cat.name + " is" + " a " + cat.age + " year old " + cat.color + " " + cat.breed + ".")
+
+function displaySum (){
+document.write('129');
+
+    document.getElementById("sum").innerHTML = Sum();
+}
+
+toUpper(name);
+add(29, 72, 28);
+
+let str = 'Hello World';
+let length = str.length;
+
+console.log(length);
